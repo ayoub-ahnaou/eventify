0b143f2a3b5675405ae3ab59bf529ba28933cee6
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-11-18 14:55:37 +0100

    Refactor environment variable loading in EventifyApplication to ignore missing .env file

--- src/main/java/com/security/eventify/EventifyApplication.java
+++ src/main/java/com/security/eventify/EventifyApplication.java
@@ -11,17 +11,15 @@ import java.util.Objects;
 public class EventifyApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EventifyApplication.class, args);
-    }
-
-    @PostConstruct
-    public void loadEnv() {
-        Dotenv dotenv = Dotenv.configure().load();
+        // Load .env first
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
 
         System.setProperty("DB_HOST", Objects.requireNonNull(dotenv.get("DB_HOST")));
         System.setProperty("DB_PORT", Objects.requireNonNull(dotenv.get("DB_PORT")));
         System.setProperty("DB_NAME", Objects.requireNonNull(dotenv.get("DB_NAME")));
         System.setProperty("DB_USER", Objects.requireNonNull(dotenv.get("DB_USER")));
         System.setProperty("DB_PASSWORD", Objects.requireNonNull(dotenv.get("DB_PASSWORD")));
+
+        SpringApplication.run(EventifyApplication.class, args);
     }
 }
