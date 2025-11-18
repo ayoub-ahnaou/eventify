2a2d5e3126c7be409d7ecbb26b5127e754ff40cc
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-11-18 13:44:11 +0100

    Add Event and Registration entity classes with relationships to User

---
+++ src/main/java/com/security/eventify/model/entity/User.java
@@ -0,0 +1,32 @@
+package com.security.eventify.model.entity;
+
+import com.security.eventify.model.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data @NoArgsConstructor @AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @OneToMany
+    private List<Registration> registrations;
+
+    @OneToMany
+    private List<Event> events;
+}
