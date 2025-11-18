a1a398b151c39c4e53212d915c40ce3ec5a955f9
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-11-18 13:44:38 +0100

    Add environment variable loading in EventifyApplication

--- src/main/java/com/security/eventify/EventifyApplication.java
+++ src/main/java/com/security/eventify/EventifyApplication.java
@@ -1,7 +1,11 @@
 package com.security.eventify;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Objects;
 
 @SpringBootApplication
 public class EventifyApplication {
@@ -10,4 +14,14 @@ public class EventifyApplication {
         SpringApplication.run(EventifyApplication.class, args);
     }
 
+    @PostConstruct
+    public void loadEnv() {
+        Dotenv dotenv = Dotenv.configure().load();
+
+        System.setProperty("DB_HOST", Objects.requireNonNull(dotenv.get("DB_HOST")));
+        System.setProperty("DB_PORT", Objects.requireNonNull(dotenv.get("DB_PORT")));
+        System.setProperty("DB_NAME", Objects.requireNonNull(dotenv.get("DB_NAME")));
+        System.setProperty("DB_USER", Objects.requireNonNull(dotenv.get("DB_USER")));
+        System.setProperty("DB_PASSWORD", Objects.requireNonNull(dotenv.get("DB_PASSWORD")));
+    }
 }
