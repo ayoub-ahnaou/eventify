e9903318f400734dac3c41dc0651cf9ff0651d6e
ismail baoud <ibaoud46@egmail.com>
2025-11-19 09:13:07 +0100

    Add method to find registrations by user ID in RegistrationRepository

--- src/main/java/com/security/eventify/repository/RegistrationRepository.java
+++ src/main/java/com/security/eventify/repository/RegistrationRepository.java
@@ -1,10 +1,12 @@
 package com.security.eventify.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.security.eventify.dto.RegistrationResponseDTO;
 import com.security.eventify.model.entity.Registration;
 
 public interface RegistrationRepository extends JpaRepository<Registration, Long> {
-
-    
+    List<RegistrationResponseDTO> findRegistrationsByUserId(Long id);
 }
