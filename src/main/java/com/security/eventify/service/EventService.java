a5cacb3500e754007c61123a6439a666e8f0085e
ismail baoud <ibaoud46@egmail.com>
2025-11-19 09:13:19 +0100

    Refactor EventService to improve event creation and deletion methods

--- src/main/java/com/security/eventify/service/EventService.java
+++ src/main/java/com/security/eventify/service/EventService.java
@@ -1,18 +1,23 @@
 package com.security.eventify.service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+
+import org.springframework.stereotype.Service;
 
 import com.security.eventify.dto.EventRequestDTO;
 import com.security.eventify.dto.EventResponseDTO;
 import com.security.eventify.mapper.EventMapper;
 import com.security.eventify.model.entity.Event;
+import com.security.eventify.model.entity.User;
 import com.security.eventify.repository.EventRepository;
 
+@Service
 public class EventService {
 
     EventRepository eventRepository;
     EventMapper eventMapper;
-
+    
     public EventService(EventRepository eventRepository , EventMapper eventMapper){
         this.eventRepository = eventRepository;
         this.eventMapper = eventMapper;
@@ -22,6 +27,9 @@ public class EventService {
     // this is the method of event creation
     public EventResponseDTO createEvent(EventRequestDTO eventDto) {
         Event event = eventMapper.dtoToEvent(eventDto);
+        event.setDateTime(LocalDateTime.now());
+        User user = new User(); // just mocke user untill i implement the login method so i can have the userDetails 
+        event.setOrganizer(user);
         EventResponseDTO response = eventMapper.eventToDto(eventRepository.save(event));
         return response;
     }
@@ -43,8 +51,7 @@ public class EventService {
 
     // this is the method of delete specifique event 
     public void deleteEvent(Long id) {
-        Event event = eventRepository.findById(id).orElseThrow(()->new IllegalArgumentException("there is no event with this id"));
-        eventRepository.delete(event);
+        eventRepository.deleteById(id);
     }
     
     // this is the method of get all events
