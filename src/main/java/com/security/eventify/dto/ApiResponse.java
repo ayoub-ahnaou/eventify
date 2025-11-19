348aea56650cfbbdd968fafa0ce14171b5549ca9
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-11-19 14:13:52 +0100

    Add ApiResponse class for standardized API response structure

---
+++ src/main/java/com/security/eventify/dto/ApiResponse.java
@@ -0,0 +1,13 @@
+package com.security.eventify.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiResponse {
+    private Integer status;
+    private String message;
+}
