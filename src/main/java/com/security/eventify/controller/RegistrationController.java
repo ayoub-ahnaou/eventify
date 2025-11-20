bbcb428b8a9ac7f764b77a6ee9370643ee084e99
ismail baoud <ibaoud46@egmail.com>
2025-11-20 10:57:00 +0100

    Refactor RegistrationController to use RegistrationService and update registration method to return ResponseEntity

--- src/main/java/com/security/eventify/controller/RegistrationController.java
+++ src/main/java/com/security/eventify/controller/RegistrationController.java
@@ -3,8 +3,9 @@ package com.security.eventify.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.security.eventify.dto.registration.response.RegistrationResponseDTO;
+import com.security.eventify.service.RegistrationService;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -13,12 +14,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 @RequestMapping("/api/user/events")
 public class RegistrationController {
     
-    RegistrationController() {
 
+    private final RegistrationService registrationService;
+    RegistrationController(RegistrationService registrationService) {
+        this.registrationService = registrationService;
     }
 
     @PostMapping("/{id}/register")
-    public void register(@PathVariable Long id) {
+    public ResponseEntity<String> createRegistration(@PathVariable("id") Long eventId) {
+        registrationService.createRegistration(eventId);
+        return ResponseEntity.ok().body("registration created succefullt");
     }
     
 }
