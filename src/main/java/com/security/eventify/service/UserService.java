42a9e4ca0f0182d3e331bd22cc28e62ef755f92e
ismail baoud <ibaoud46@egmail.com>
2025-11-20 10:57:30 +0100

    Refactor UserService interface to include missing method signatures for user retrieval and role updates

--- src/main/java/com/security/eventify/service/UserService.java
+++ src/main/java/com/security/eventify/service/UserService.java
@@ -1,8 +1,17 @@
 package com.security.eventify.service;
 
+import java.util.List;
+
 import com.security.eventify.dto.user.request.CreateUserRequest;
+import com.security.eventify.dto.user.request.UpdateRoleRequest;
 import com.security.eventify.dto.user.response.UserResponse;
 
 public interface UserService {
     UserResponse create(CreateUserRequest dto);
+
+    List<UserResponse> getAllUsers();
+
+    UserResponse updateRole(Long id, UpdateRoleRequest roleRequest);
+
+    UserResponse getUserById(Long id);
 }
