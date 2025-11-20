07606859220af706a903fe8ffc55e8a53c00f94f
ismail baoud <ibaoud46@egmail.com>
2025-11-20 10:57:52 +0100

    Refactor UserServiceImpl to enhance user retrieval methods and improve role update functionality

--- src/main/java/com/security/eventify/service/implementation/UserServiceImpl.java
+++ src/main/java/com/security/eventify/service/implementation/UserServiceImpl.java
@@ -1,11 +1,15 @@
 package com.security.eventify.service.implementation;
 
 import com.security.eventify.dto.user.request.CreateUserRequest;
+import com.security.eventify.dto.user.request.UpdateRoleRequest;
 import com.security.eventify.dto.user.response.UserResponse;
 import com.security.eventify.mapper.UserMapper;
 import com.security.eventify.model.entity.User;
 import com.security.eventify.repository.UserRepository;
 import com.security.eventify.service.UserService;
+
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 @Service
@@ -23,4 +27,25 @@ public class UserServiceImpl implements UserService {
         User user = userMapper.toEntity(dto);
         return this.userMapper.toDto(this.userRepository.save(user));
     }
+
+
+    @Override
+    public List<UserResponse> getAllUsers() {
+        return userRepository.findAll().stream().map(u -> userMapper.toDto(u)).toList();
+    }
+
+    @Override
+    public UserResponse updateRole(Long id, UpdateRoleRequest roleRequest) {
+        User user = userRepository.findById(id).orElseThrow(()-> new RuntimeException("there is no user with this id"));
+        user.setRole(roleRequest.getRole());
+        return userMapper.toDto(userRepository.save(user));
+    }
+
+    @Override
+    public UserResponse getUserById(Long id) {
+        User user = userRepository.findById(id).orElseThrow(()-> new RuntimeException("there is no user with this id"));
+        return userMapper.toDto(user);
+    }
+
+    
 }
