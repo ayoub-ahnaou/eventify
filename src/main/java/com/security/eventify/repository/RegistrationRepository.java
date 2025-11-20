564fdc2184f808b5b549604e66fe0224c98c258a
ismail baoud <ibaoud46@egmail.com>
2025-11-20 10:57:19 +0100

    Refactor RegistrationRepository to return Registration entities instead of RegistrationResponseDTO

--- src/main/java/com/security/eventify/repository/RegistrationRepository.java
+++ src/main/java/com/security/eventify/repository/RegistrationRepository.java
@@ -4,11 +4,10 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.security.eventify.dto.registration.response.RegistrationResponseDTO;
 import com.security.eventify.model.entity.Registration;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RegistrationRepository extends JpaRepository<Registration, Long> {
-    List<RegistrationResponseDTO> findRegistrationsByUserId(Long id);
+    List<Registration> findByUserId(Long userId);
 }
