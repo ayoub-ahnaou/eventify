1bdc610433ead657f4e1bed332f8c4a62ae4b714
ismail baoud <ibaoud46@egmail.com>
2025-11-20 10:57:10 +0100

    Enhance RegistrationResponseDTO with Lombok annotations for cleaner code and improved readability

--- src/main/java/com/security/eventify/dto/registration/response/RegistrationResponseDTO.java
+++ src/main/java/com/security/eventify/dto/registration/response/RegistrationResponseDTO.java
@@ -1,7 +1,17 @@
 package com.security.eventify.dto.registration.response;
 
-public class RegistrationResponseDTO {
-    
+import com.security.eventify.model.entity.Event;
+import com.security.eventify.model.entity.User;
 
-    
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegistrationResponseDTO {
+    private String status;
+    private User user;
+    private Event event;
 }
