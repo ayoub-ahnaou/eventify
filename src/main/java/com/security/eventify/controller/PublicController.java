443e17ceddbbef7fc61117ff6f42652b1b575b19
ismail baoud <ibaoud46@egmail.com>
2025-11-20 10:56:55 +0100

    Add EventService dependency and implement getAllEvents method in PublicController

--- src/main/java/com/security/eventify/controller/PublicController.java
+++ src/main/java/com/security/eventify/controller/PublicController.java
@@ -2,11 +2,17 @@ package com.security.eventify.controller;
 
 import com.security.eventify.dto.ApiResponse;
 import com.security.eventify.dto.ApiResponseSuccess;
+import com.security.eventify.dto.event.response.EventResponseDTO;
 import com.security.eventify.dto.user.request.CreateUserRequest;
 import com.security.eventify.dto.user.response.UserResponse;
+import com.security.eventify.service.EventService;
 import com.security.eventify.service.UserService;
 import jakarta.validation.Valid;
+
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,9 +23,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class PublicController {
 
     private final UserService userService;
+    private final EventService eventService;
 
-    public PublicController(UserService userService) {
+    public PublicController(UserService userService, EventService eventService) {
         this.userService = userService;
+        this.eventService = eventService;
     }
 
     @PostMapping("/users")
@@ -28,5 +36,11 @@ public class PublicController {
         ApiResponseSuccess<UserResponse> apiResponse = new ApiResponseSuccess<>(201, "User registered successfully", user);
 
         return ResponseEntity.status(201).body(apiResponse);
+    }
+
+
+    @GetMapping("/events")
+    public List<EventResponseDTO> getAllEvents() {
+        return eventService.getAllEvents();
     }
 }
