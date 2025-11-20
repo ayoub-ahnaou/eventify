e83937e2d4169f4db0f70b68edd1c2938c944482
ismail baoud <ibaoud46@egmail.com>
2025-11-20 10:57:04 +0100

    Refactor UserController to improve user profile retrieval and ensure proper response handling

--- src/main/java/com/security/eventify/controller/UserController.java
+++ src/main/java/com/security/eventify/controller/UserController.java
@@ -4,10 +4,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.security.eventify.dto.registration.response.RegistrationResponseDTO;
+import com.security.eventify.dto.user.response.UserResponse;
 import com.security.eventify.service.RegistrationService;
-
+import com.security.eventify.service.UserService;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -18,27 +20,25 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class UserController {
 
 
-    // UserService UserService;
+    UserService userService;
     RegistrationService registrationService;
 
     UserController(
-        // UserService userService,
+        UserService userService,
         RegistrationService registrationService
     ) {
-        // this.userService = userService;
+        this.userService = userService;
         this.registrationService = registrationService;
     }
 
     
     @GetMapping("/profile/{id}")
-    public String profile(@PathVariable("id") Long id) {
-        return new String();
+    public ResponseEntity<UserResponse> profile(@PathVariable("id") Long id) {
+        return ResponseEntity.ok().body(userService.getUserById(id));
     }
 
     @GetMapping("/registrations/{id}")
     public List<RegistrationResponseDTO> userRegistrations(@PathVariable("id") Long id) {
         return registrationService.getAllRegistrationsOfUser(id);
     }
-    
-    
 }
