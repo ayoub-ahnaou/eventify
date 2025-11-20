e21f8df1b5dddf001020eb0e6e108f3a04120780
ismail baoud <ibaoud46@egmail.com>
2025-11-20 10:56:45 +0100

    Add AdminController for user management and role updates

---
+++ src/main/java/com/security/eventify/controller/AdminController.java
@@ -0,0 +1,43 @@
+package com.security.eventify.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.security.eventify.dto.user.request.UpdateRoleRequest;
+import com.security.eventify.dto.user.response.UserResponse;
+import com.security.eventify.service.EventService;
+import com.security.eventify.service.UserService;
+
+@RestController
+@RequestMapping("/api/admin")
+public class AdminController {
+    
+
+    private final UserService userService;
+    private final EventService eventService;
+
+    AdminController(UserService userService, EventService eventService) {
+        this.userService = userService;
+        this.eventService = eventService;
+    }
+
+
+    @GetMapping("/users")
+    public ResponseEntity<List<UserResponse>> getUsers() {
+        List users = userService.getAllUsers();
+        return ResponseEntity.ok().body(users);    
+    }
+
+
+    @PutMapping("/users/{id}/role")
+    public UserResponse UpdateRole(@PathVariable Long id, @RequestBody UpdateRoleRequest roleRequest) {
+        return userService.updateRole(id , roleRequest);
+    }
+}
