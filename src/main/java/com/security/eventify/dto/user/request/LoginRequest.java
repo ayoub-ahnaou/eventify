f54b30c64c838fcc874f4427b770f7b983bd8f6b
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-11-21 10:09:24 +0100

    Add LoginRequest DTO for user login with email and password validation

---
+++ src/main/java/com/security/eventify/dto/user/request/LoginRequest.java
@@ -0,0 +1,17 @@
+package com.security.eventify.dto.user.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequest {
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
+}
