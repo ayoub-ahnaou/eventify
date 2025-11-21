b1947e9f17d8df58919a6a3673005a037f07c48f
ismail baoud <ibaoud46@egmail.com>
2025-11-21 22:53:42 +0100

    Add role-based access control to RegistrationController for User role

--- src/main/java/com/security/eventify/controller/RegistrationController.java
+++ src/main/java/com/security/eventify/controller/RegistrationController.java
@@ -6,14 +6,15 @@ import org.springframework.web.bind.annotation.RestController;
 import com.security.eventify.service.RegistrationService;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
 @RestController
 @RequestMapping("/api/user/events")
+@PreAuthorize("hasRole('User')")
 public class RegistrationController {
-    
 
     private final RegistrationService registrationService;
     RegistrationController(RegistrationService registrationService) {
