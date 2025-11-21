e310ee25f8cc2c0db4756071bc1a7c48ad800c72
ismail baoud <ibaoud46@egmail.com>
2025-11-21 22:53:47 +0100

    Add a blank line for improved readability in RegistrationRequestDTO

--- src/main/java/com/security/eventify/dto/registration/request/RegistrationRequestDTO.java
+++ src/main/java/com/security/eventify/dto/registration/request/RegistrationRequestDTO.java
@@ -1,6 +1,7 @@
 package com.security.eventify.dto.registration.request;
 
 
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
